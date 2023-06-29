4639970138593c00a3f198501b292939717edb95
Luis D. Maco <luisdmaco@outlook.com>
2023-06-29 17:53:59 -0500

    AppException added

---
+++ PCInfo_backend/PCInfo/Security/Exceptions/AppException.cs
@@ -0,0 +1,8 @@
+﻿namespace PCInfo_backend.PCInfo.Security.Exceptions;
+
+public class AppException : Exception
+{
+    public AppException() : base() { }
+    public AppException(string message) : base(message) { }
+    public AppException(string message, Exception innerException) : base(message, innerException) { }
+}
