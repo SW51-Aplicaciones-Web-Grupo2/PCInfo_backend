7ab14cd4df4aedc50e2e9726d7acd3f14ff66861
Luis D. Maco <luisdmaco@outlook.com>
2023-06-29 18:17:39 -0500

    Methods UserService added

--- PCInfo_backend/PCInfo/Security/Services/UserService.cs
+++ PCInfo_backend/PCInfo/Security/Services/UserService.cs
@@ -3,6 +3,10 @@ using PCInfo_backend.PCInfo.Domain.Repositories;
 using PCInfo_backend.PCInfo.Security.Domain.Models;
 using PCInfo_backend.PCInfo.Security.Domain.Services;
 using PCInfo_backend.PCInfo.Security.Domain.Services.Communication;
+using PCInfo_backend.PCInfo.Security.Exceptions;
+using BCryptNet = BCrypt.Net.BCrypt;
+using IUserRepository = PCInfo_backend.PCInfo.Security.Domain.Repositories.IUserRepository;
+
 
 namespace PCInfo_backend.PCInfo.Security.Services;
 
@@ -25,28 +29,83 @@ public class UserService : IUserService
         throw new NotImplementedException();
     }
 
-    public Task<IEnumerable<User>> ListAsync()
+    public async Task<IEnumerable<User>> ListAsync()
     {
-        throw new NotImplementedException();
+        return await _userRepository.ListAsync();
     }
 
-    public Task<User> GetByIdAsync(int id)
+    public async Task<User> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = await _userRepository.FindByIdAsync(id);
+        if (user == null) throw new KeyNotFoundException("User not found.");
+        return user;
     }
 
-    public Task RegisterAsync(RegisterRequest model)
+    public async Task RegisterAsync(RegisterRequest model)
     {
-        throw new NotImplementedException();
+        if (_userRepository.ExistsByUsername(model.Username))
+            throw new AppException("Username " + model.Username + " is already taken");
+        
+        var user = _mapper.Map<User>(model);
+        
+        user.PasswordHash = BCryptNet.HashPassword(model.Password);
+
+        try
+        {
+            await _userRepository.AddAsync(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while saving the user: {e.Message}");
+        }
+    }
+    
+    // helper methods
+    private User GetById(int id)
+    {
+        var user = _userRepository.FindByIdAsync(id);
+        if (user == null) throw new KeyNotFoundException("User not found.");
+        return user.Result;
     }
 
-    public Task UpdateAsync(int id, UpdateRequest model)
+    public async Task UpdateAsync(int id, UpdateRequest model)
     {
-        throw new NotImplementedException();
+        var user = GetById(id);
+        
+        // Validate
+        if (_userRepository.ExistsByUsername(model.Username))
+            throw new AppException("Username " + model.Username + " is already taken");
+        
+        // Hash password if it was entered
+        if (!string.IsNullOrEmpty(model.Password))
+            user.PasswordHash = BCryptNet.HashPassword(model.Password);
+        
+        // Copy model to user and save
+        _mapper.Map(model, user);
+        try
+        {
+            _userRepository.Update(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while updating the user: {e.Message}");
+        }
     }
 
-    public Task DeleteAsync(int id)
+    public async Task DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = GetById(id);
+
+        try
+        {
+            _userRepository.Remove(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while deleting the user: {e.Message}");
+        }
     }
 }
