c66a0b806ba8239c0a253d57bd9d3e7fff183ae7
Luis D. Maco <luisdmaco@outlook.com>
2023-06-29 17:52:16 -0500

    UserService added

---
+++ PCInfo_backend/PCInfo/Security/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using PCInfo_backend.PCInfo.Domain.Repositories;
+using PCInfo_backend.PCInfo.Security.Domain.Models;
+using PCInfo_backend.PCInfo.Security.Domain.Services;
+using PCInfo_backend.PCInfo.Security.Domain.Services.Communication;
+
+namespace PCInfo_backend.PCInfo.Security.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+
+    public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public Task<AuthenticateResponse> Authenticate(AuthenticateRequest model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RegisterAsync(RegisterRequest model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(int id, UpdateRequest model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
